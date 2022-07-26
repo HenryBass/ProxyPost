9c255f565138d29f0a476eb6acdd386aff92d3ad
HenryBass <63601449+HenryBass@users.noreply.github.com>
2022-07-26 12:34:00 -0400

    g

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ var qs = require('querystring');
 var bodyParser = require('body-parser')
 const Jimp = require("jimp");
 const crypto = require("crypto");
+var mod = false
+var lasttime = Math.floor(Date.now()/1000);
+console.log(lasttime)
 
 const ig = new IgApiClient();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -24,7 +27,7 @@ app.listen(port, () => {
 });
 
 function wrap(text, length) {
-  out = ""
+  let out = ""
   let words = text.split(" ")
   let count = 0
   for (let i = 0; i < words.length; i++) {
@@ -61,13 +64,23 @@ function wrap(text, length) {
   });
   
   app.post('/', urlencodedParser, (req, res) => {
-    const hasher = crypto.createHmac("sha256", process.env.SALT);
-
-    msg = req.body.msg
+    
+    mod = false
+    let  msg = req.body.msg
+    var key = req.body.key
     var ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress);
+    const hasher = crypto.createHmac("sha256", process.env.SALT);
     var hash = hasher.update(ip).digest("hex")
+    
+    const keyhasher = crypto.createHmac("sha256", process.env.SALT);
+    var keyhash = keyhasher.update(key).digest("hex")
+
+    if (keyhash == process.env.KEY) {
+      mod = true
+    }
+
     console.log(msg + ' Hashed to: ' + hash)
-    id = fs.readFileSync('logs.txt', 'utf8')
+    let id = fs.readFileSync('logs.txt', 'utf8')
     id = id.split("%%\n").length
   
     log(`id:${id}, msg:${msg}, hash:${hash}`)
@@ -77,10 +90,18 @@ function wrap(text, length) {
     if (bans.includes(hash)) {
       res.sendFile(__dirname + "/views/ban.html")
       console.log("Account is banned")
-    } else {
-      res.sendFile(__dirname + "/views/sent.html")
-      console.log("Image Generated")
-    makeImg(id + ": " + msg)
+    } else if ((Math.floor(Date.now()/1000) - lasttime) <= 20) {
+      res.sendFile(__dirname + "/views/ratelimited.html")
+      console.log("RateLimited")
+    }
+    else if (typeof msg == "string") {
+      if(msg.length >= 3) {
+        res.sendFile(__dirname + "/views/sent.html")
+        console.log("Image Generated")
+        lasttime = Math.floor(Date.now()/1000)
+        makeImg(id + ": " + msg)
+      }
+
     }
 
   });
@@ -90,7 +111,6 @@ function log(newLog) {
 }
 
 function png2jpg(msg) {
-
   Jimp.read('post.png').then(img => {
     img.write('post.jpg');
   }).then(() => {
@@ -108,12 +128,22 @@ function post(msg) {
 }
 
 function makeImg(message) {
-  ctx.fillStyle = 'rgb(50, 50, 75)';
+  if (mod == false) {
+    ctx.fillStyle = 'rgb(50, 75, 50)';
+  } else {
+    ctx.fillStyle = 'rgb(75, 50, 50)'
+  }
+  
   ctx.fillRect(0, 0, 1080, 1080)
 
-  text = wrap(message, 27)
+  let text = wrap(message, 27)
   ctx.font = '48px monospace';
-  ctx.fillStyle = 'rgb(200, 255, 255)';
+  if (mod == false) {
+    ctx.fillStyle = 'rgb(200, 255, 200)';
+  } else {
+    ctx.fillStyle = 'rgb(255, 200, 200)';
+  }
+  
   ctx.fillText(text, 10, 48);
   const buffer = canvas.toBuffer('image/png')
   fs.writeFileSync('./post.png', buffer)
